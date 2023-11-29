4b63bea19d3b62a42796e638cbf4c5df6c5d256b
Vladislav <labkovich.vl@gmail.com>
2023-11-29 22:40:16 +0300

    feat: add new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {findConfigFile} from 'typescript';
+import Accordion from './components/Accordion/Accordion';
+import {Rating} from './components/Rating/Rating';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function hello() {
+    alert('Hello it-kamasutra')
+}
+
+// hello();
+
+function App(props: any) {
+    console.log("App rendering")
+    return (
+        <div>
+            {/*<img src={"https://www.svgrepo.com/show/374032/reactjs.svg"}/>*/}
+            <PageTitle title={"This is APP component"} />
+            <PageTitle title={"My friends"}/>
+            {/*Article 1*/}
+            {/*<Rating value={3}/>*/}
+            <Accordion titleValue={"Menu"} collapsed={false}/>
+            <Accordion titleValue={"Users"} collapsed={true}/>
+
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+
+        </div>
+    );
+}
+
+type PageTitlePropsType = {
+    title: string;
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    console.log("PageTitle rendering")
+    return <h1>{props.title}</h1>
+
 }
 
 export default App;
