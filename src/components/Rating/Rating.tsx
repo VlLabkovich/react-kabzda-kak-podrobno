91c5231f29e5855c1772aedda3c35b5b68193345
Vladislav <labkovich.vl@gmail.com>
2023-11-29 22:44:59 +0300

    feat: add new components

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,23 +1,89 @@
 import React from "react"
 
-export function Rating() {
-    console.log("Rating rendering")
-    return (
-        <div>
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-        </div>
-    )
+type RatingPropsType = {
+    value: number
 }
 
-function Star() {
-    console.log("Star rendering")
+export function Rating(props: RatingPropsType) {
+    debugger
+    console.log("Rating rendering")
+    if (props.value === 1) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        );
+    }
+    if (props.value === 2) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        );
+    }
+    if (props.value === 3) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+                <Star selected={false}/>
+            </div>
+        );
+    }
+    if (props.value === 4) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={false}/>
+            </div>
+        );
+    }
+    if (props.value === 5) {
+        return (
+            <div>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+                <Star selected={true}/>
+            </div>
+        );
+    }
     return (
-        <span>star</span>
-    )
+        <div>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+        </div>
+    );
+}
+
+type StarPropsType = {
+    selected: boolean
+}
+
+function Star(props: StarPropsType) {
+    console.log("Star rendering")
+    if (props.selected === true) {
+        return <span><b>star</b></span>
+    } else {
+        return <span>star</span>
+    }
 }
 
 //   export default Rating
