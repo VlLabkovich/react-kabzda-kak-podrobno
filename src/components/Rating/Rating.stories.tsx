6b8ad29394a033ab7a47e97ad5aa2f738854cdcc
Vladislav <labkovich.vl@gmail.com>
2024-05-19 19:06:15 +0300

    feat: add rating stories

---
+++ src/components/Rating/Rating.stories.tsx
@@ -0,0 +1,23 @@
+import { action } from '@storybook/addon-actions';
+import {Rating, RatingValueType} from "./Rating";
+import React, {useState} from "react";
+
+export default {
+    title: 'Rating stories',
+    component: Rating,
+}
+
+export const EmptyRating = () => <Rating value={0} onClick={()=>{}}/>
+
+export const Rating1 = () => <Rating value={1} onClick={()=>{}}/>
+export const Rating2 = () => <Rating value={2} onClick={()=>{}}/>
+export const Rating3 = () => <Rating value={3} onClick={()=>{}}/>
+export const Rating4 = () => <Rating value={4} onClick={()=>{}}/>
+export const Rating5 = () => <Rating value={5} onClick={()=>{}}/>
+
+export const ChangeRating = () => {
+    const [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+    return <Rating
+        value={ratingValue}
+        onClick={setRatingValue}/>
+}
