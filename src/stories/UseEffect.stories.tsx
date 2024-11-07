0d18fd0f05085b5965d8da79d9aca086884b6416
Vladislav <labkovich.vl@gmail.com>
2024-11-07 18:00:06 +0300

    feat: add cleanUp for UseEffect

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -33,33 +33,111 @@ export default {
 //     </>
 // }
 
+// export const SetIntervalExample = () => {
+//
+//     const [counter, setCounter] = useState(1)
+//     const [fake, setFake] = useState(0)
+//
+//     console.log("SetIntervalExample");
+//
+//     // useEffect(() => {
+//     //     setTimeout(() => {
+//     //         document.title = counter.toString()
+//     //     }, 1000)
+//     //     console.log("useEffect render")
+//     // }, [counter]);
+//
+//     useEffect(() => {
+//
+//         console.log('setInterval')
+//
+//         setInterval(() => {
+//             setCounter((state) => state + 1)
+//         }, 1000)
+//
+//     }, []);
+//
+//     return <>
+//         Counter: {counter}, Fake: {fake}
+//         {/*<button onClick={() => setCounter(state => state + 1)}>+</button>*/}
+//         {/*<button onClick={() => setFake(state => state + 1)}>fake</button>*/}
+//     </>
+// }
+
+// export const ResetUseEffectExample = () => {
+//
+//     const [counter, setCounter] = useState(0)
+//
+//
+//     console.log("component rendered");
+//
+//     // useEffect(() => {
+//     //     setTimeout(() => {
+//     //         document.title = counter.toString()
+//     //     }, 1000)
+//     //     console.log("useEffect render")
+//     // }, [counter]);
+//
+//     useEffect(() => {
+//         console.log('Effect occured')
+//     }, []);
+//
+//     const increase = () => {
+//         setCounter(state => state + 1)
+//     }
+//
+//     return <>
+//         Hello, counter: {counter}
+//         <button onClick={increase}>Click</button>
+//     </>
+// }
+
+// export const KeyTrackerExample = () => {
+//
+//     const [counter, setCounter] = useState(0)
+//
+//     const [text, setText] = useState('')
+//
+//     console.log("Component rendered");
+//
+//     useEffect(() => {
+//         const handler = (e: KeyboardEvent) => {
+//             setText(text + e.key)
+//             console.log('UseEffect rendered')
+//         }
+//         window.addEventListener('keypress', handler)
+//         return () => {
+//             window.removeEventListener('keypress', handler);
+//         }
+//     }, [text]);
+//
+//     return (
+//         <div>
+//             Hello, counter: {text}
+//             <hr/>
+//             <button onClick={() => setCounter(counter => counter + 1)}>PUSH</button>
+//             : {counter}
+//         </div>
+//     )
+// }
+
 export const SetTimeoutExample = () => {
 
-    const [counter, setCounter] = useState(1)
-    const [fake, setFake] = useState(0)
+    const [text, setText] = useState('')
 
-    console.log("SetTimeoutExample");
-
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         document.title = counter.toString()
-    //     }, 1000)
-    //     console.log("useEffect render")
-    // }, [counter]);
+    console.log("Component rendered " + text);
 
     useEffect(() => {
 
-        console.log('setInterval')
+        console.log('setTimeout')
 
-        setInterval(() => {
-            setCounter((state) => state + 1)
-        }, 1000)
-
-    }, []);
+        const timeoutId = setTimeout(() => {
+            setText('rendered 3 second')
+        }, 3000)
+        return ()=> clearTimeout(timeoutId)
+    }, [text]);
 
     return <>
-        Counter: {counter}, Fake: {fake}
-        {/*<button onClick={() => setCounter(state => state + 1)}>+</button>*/}
-        {/*<button onClick={() => setFake(state => state + 1)}>fake</button>*/}
+        Text: {text}
     </>
 }
